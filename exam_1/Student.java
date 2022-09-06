6d9ae19401b29d40da67a16729b67094296ab6d6
Fortune Raphael C. Bermudez <111546354+bermudezfc@users.noreply.github.com>
2022-09-06 09:29:53 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,29 @@
+public class Student {
+
+// Properties
+String surname;
+String firstName;
+char middleInitial;
+String dateOfBirth;
+int studentNumber;
+String studentEmailAddress;
+boolean iAmAwesome;
+
+// Methods
+public void sayMyStudentNumber(){
+    System.out.println("My student number is: " + studentNumber);
+}
+
+public void sayMyEmailAddress(){
+    System.out.println("My student email is: " + studentEmailAddress);
+}
+
+public void amIAwesome(){
+    System.out.println(iAmAwesome + ", I am Very Awesome!");
+}
+
+
+
+
+}
+
